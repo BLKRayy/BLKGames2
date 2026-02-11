0e458154ff26279548850552bdfbf51934c9b48b
BLKRayy <Kreal1274@gmail.com>
2026-02-10 21:16:13 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,7 @@
-const CACHE_NAME = "blk-arcade-cache-v1";
-
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME));
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  const url = new URL(e.request.url);
-
-  if (url.searchParams.get("recent") === "1") {
-    e.respondWith(
-      caches.match(e.request).then(res => {
-        return (
-          res ||
-          fetch(e.request).then(networkRes => {
-            caches.open(CACHE_NAME).then(cache => {
-              cache.put(e.request, networkRes.clone());
-            });
-            return networkRes;
-          })
-        );
-      })
-    );
-  }
+self.addEventListener("activate", event => {
+  event.waitUntil(clients.claim());
 });
